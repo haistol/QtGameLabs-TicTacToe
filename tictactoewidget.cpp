5a229a7a610aebfb7291f45281da76587309b50b
Juan De la Rosa <juan.d@futurelinkcorporation.com>
2018-07-19 09:24:12 -0400

    construc the game board in the tictactoewidget class

--- tictactoewidget.cpp
+++ tictactoewidget.cpp
@@ -1,6 +1,16 @@
 #include "tictactoewidget.h"
 
+
+
 TicTacToeWidget::TicTacToeWidget(QWidget *parent) : QWidget(parent)
 {
+    QGridLayout *gridLayout = new QGridLayout(this);
 
+    for (int row = 0; row < 3; ++row) {
+        for (int column = 0; column < 3; ++column) {
+            QPushButton *button = new QPushButton(" ");
+            gridLayout->addWidget(button, row, column);
+            m_board.append(button);
+        }
+    }
 }
--- tictactoewidget.h
+++ tictactoewidget.h
@@ -1,13 +1,17 @@
 #ifndef TICTACTOEWIDGET_H
 #define TICTACTOEWIDGET_H
 
+#include <QPushButton>
 #include <QWidget>
+#include <QGridLayout>
 
 class TicTacToeWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit TicTacToeWidget(QWidget *parent = nullptr);
+private:
+    QVector<QPushButton*> m_board;
 
 signals:
 
